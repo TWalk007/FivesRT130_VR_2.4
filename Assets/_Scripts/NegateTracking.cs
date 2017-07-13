0546eb4c0c6d3f9faaebea48c8510d709c0a8506
TWalk007 <walkertb03@gmail.com>
2017-07-13 09:31:37 -0400

    Initial commit

---
+++ Assets/_Scripts/NegateTracking.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+
+public class NegateTracking : MonoBehaviour {
+
+    public GameObject cameraObject;
+    public Transform hotSpotSpindleTransform;
+    public Transform hotSpotOperatorTransform;
+
+    public bool spindleScene = false;
+    public bool operatorScene = false;
+
+    private Camera cam;
+    private Vector3 currentPos;
+
+    private void Start()
+    {
+        cam = cameraObject.GetComponent<Camera>();
+    }
+
+    void Update () {
+        
+        transform.rotation = Quaternion.Inverse(InputTracking.GetLocalRotation(VRNode.CenterEye));
+
+        if (spindleScene)
+        {
+            Quaternion targetRot = Quaternion.Euler(5f, -77f, 0f);
+            cam.transform.rotation = targetRot;
+        }
+
+        if (operatorScene)
+        {
+            Quaternion targetRot = Quaternion.Euler(5f, -150f, 0f);
+            cam.transform.rotation = targetRot;
+        }
+
+
+    }
+
+}
